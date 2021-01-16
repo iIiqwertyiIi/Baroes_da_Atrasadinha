42c71ebc9f43524f6361d4c8e983eead3fafce95
GuyNex <macorreia123456@gmail.com>
2021-01-16 16:28:36 -0300

    user view

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   get 'signup' => 'users#new', :as => 'signup'
   resources :users
   resources :sessions
-  root :to => 'home#index'
+  root 'application#homepage'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
