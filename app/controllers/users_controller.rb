e109a77aaef8d1e4c167e8dade14149b041ad3d7
GuyNex <macorreia123456@gmail.com>
2021-01-20 17:54:06 -0300

    username and admin added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,12 @@ class UsersController < ApplicationController
     @user =  User.new
   end
 
+  def edit
+    @user = current_user
+  end
+
+  def show; end
+
   def create
     @user = User.new(user_args)
     if @user.save
@@ -11,6 +17,18 @@ class UsersController < ApplicationController
       render :new
     end
   end
+
+  def update
+    begin
+      current_user.update!(user_args)
+      image_change(current_user)
+      flash[:notice] = 'Usuário editado com sucesso'
+      redirect_to perfil_user_path
+    rescue => err
+      flash[:notice] = err
+    end
+  end
+
   private
   def user_args
     params.require(:user).permit(:username, :password, :email, :password_confirmation)
