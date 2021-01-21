9df5c6121a5ebb08905348d034cb393c6e35e4f7
GuyNex <macorreia123456@gmail.com>
2021-01-21 18:32:01 -0300

    adm permission

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   def homepage; end
+
+  def admin_authority
+    if current_user.nil?
+      flash[:notice] = "Por favor, Crie uma conta ou Entre para acessar a pagina"
+      redirect_to signup_path
+    elsif current_user.admin? == false
+      flash[:warning] = "Apenas Ademiro"
+      redirect_to root_path
+      end
+  end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def is_admin?
+    if current_user.admin? == true
+      return true
+    else
+      return false
+    end
+  end
 end
