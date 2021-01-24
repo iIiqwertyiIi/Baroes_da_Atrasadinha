1c9af3b7a8879adcfe071ce4fe901439fffcb679
GuyNex <macorreia123456@gmail.com>
2021-01-24 18:10:53 -0300

    bug fixes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_21_211110) do
+ActiveRecord::Schema.define(version: 2021_01_24_194012) do
 
   create_table "genres", force: :cascade do |t|
     t.string "genres"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 2021_01_21_211110) do
     t.datetime "updated_at", null: false
     t.string "remember_me_token"
     t.datetime "remember_me_token_expires_at"
+    t.integer "user_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["remember_me_token"], name: "index_users_on_remember_me_token"
   end
